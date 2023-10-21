760ccf018c990f15a0bf4d695a4043141b7d8017
unknown <wknowpark797@gmail.com>
2023-10-21 15:59:26 +0900

    Gugudan.jsx 함수 컴포넌트 생성

---
+++ 1.구구단/Gugudan.jsx
@@ -0,0 +1,47 @@
+const React = require('react');
+const { useState, useRef } = React;
+
+const Gugudan = () => {
+	const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+	const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+	const [value, setValue] = useState('');
+	const [result, setResult] = useState('');
+	const inputRef = useRef(null);
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		if (parseInt(value) === first * second) {
+			setFirst(Math.ceil(Math.random() * 9));
+			setSecond(Math.ceil(Math.random() * 9));
+			setValue('');
+			setResult(value + ' 정답~!');
+			inputRef.current.focus();
+		} else {
+			setValue('');
+			setResult('땡!');
+			inputRef.current.focus();
+		}
+	};
+
+	const onChange = (e) => {
+		setValue(e.target.value);
+	};
+
+	return (
+		<React.Fragment>
+			<div>
+				{first} 곱하기 {second}는?
+			</div>
+
+			<form onSubmit={onSubmit}>
+				<input ref={inputRef} type='number' value={value} onChange={onChange} placeholder='정답 입력' />
+				<button type='submit'>입력</button>
+			</form>
+
+			<div>{result}</div>
+		</React.Fragment>
+	);
+};
+
+module.exports = Gugudan;
