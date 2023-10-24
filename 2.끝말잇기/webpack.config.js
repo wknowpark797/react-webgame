220e05ee87ae017ceff389e8c15e233667e52a43
unknown <wknowpark797@gmail.com>
2023-10-24 19:28:34 +0900

    핫 리로딩 설치 및 설정 적용

--- 2.끝말잇기/webpack.config.js
+++ 2.끝말잇기/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
 	name: 'word-relay-setting',
@@ -11,25 +12,39 @@ module.exports = {
 	entry: {
 		app: './client',
 	}, // 입력
-
 	module: {
 		rules: [
 			{
 				test: /\.jsx?$/,
 				loader: 'babel-loader', // entry의 파일들에 babel-loader를 적용한다.
 				options: {
-					presets: ['@babel/preset-env', '@babel/preset-react'],
-					plugins: ['@babel/plugin-proposal-class-properties'],
+					presets: [
+						[
+							'@babel/preset-env',
+							{
+								targets: {
+									browsers: ['> 5% in KR', 'last 2 chrome versions'],
+								},
+								debug: true,
+							},
+						],
+						'@babel/preset-react',
+					],
+					plugins: ['@babel/plugin-proposal-class-properties', 'react-refresh/babel'],
 				},
 			},
 		],
 	},
-
+	plugins: [new ReactRefreshWebpackPlugin()],
 	output: {
 		// __dirname: 현재 폴더 경로에 dist 생성
 		path: path.join(__dirname, 'dist'),
 		filename: 'app.js',
+		publicPath: '/dist',
 	}, // 출력
+	devServer: {
+		devMiddleware: { publicPath: '/dist' },
+		static: { directory: path.resolve(__dirname) },
+		hot: true,
+	},
 };
-
-// 'webpack' 명령어로 실행
