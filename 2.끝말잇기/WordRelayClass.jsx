45d27ac99484aa0c51c1ab85812fad2795cc8b3b
unknown <wknowpark797@gmail.com>
2023-10-24 11:17:03 +0900

    WordRelayClass 클래스 컴포넌트 화면 출력

--- 2.끝말잇기/WordRelayClass.jsx
+++ 2.끝말잇기/WordRelayClass.jsx
@@ -3,11 +3,50 @@ const { Component } = React;
 
 class WordRelayClass extends Component {
 	state = {
-		text: 'Hello webpack',
+		word: '자동차',
+		value: '',
+		result: '',
+	};
+
+	onSubmitForm = (e) => {
+		e.preventDefault();
+
+		if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+			this.setState({
+				word: this.state.value,
+				value: '',
+				result: '딩동댕',
+			});
+			this.input.focus();
+		} else {
+			this.setState({
+				value: '',
+				result: '땡',
+			});
+			this.input.focus();
+		}
+	};
+
+	onChangeInput = (e) => {
+		this.setState({ value: e.target.value });
+	};
+
+	input;
+	onRefInput = (c) => {
+		this.input = c;
 	};
 
 	render() {
-		return <div>{this.state.text}</div>;
+		return (
+			<>
+				<div>{this.state.word}</div>
+				<form onSubmit={this.onSubmitForm}>
+					<input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+					<button>입력</button>
+				</form>
+				<div>{this.state.result}</div>
+			</>
+		);
 	}
 }
 
