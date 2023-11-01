78896929289709f09c7d47ceda741ea5c9486553
unknown <wknowpark797@gmail.com>
2023-11-01 14:47:27 +0900

    화면 클릭시 setTimeout을 사용하여 화면 전환 적용

--- 4.반응속도체크/ResponseCheckClass.jsx
+++ 4.반응속도체크/ResponseCheckClass.jsx
@@ -2,12 +2,43 @@ import React, { Component } from 'react';
 
 class ResponseCheckClass extends Component {
 	state = {
-		state: 'waiting', // 배경색 상태 - waiting, ready, now
+		state: 'waiting', // 배경색 상태 - waiting(파란색), ready(빨간색), now(초록색)
 		message: '클릭해서 시작하세요.',
 		result: [],
 	};
 
-	onClickScreen = () => {};
+	timeout;
+
+	onClickScreen = () => {
+		const { state, message, result } = this.state;
+
+		if (state === 'waiting') {
+			this.setState({
+				state: 'ready',
+				message: '초록색이 되면 클릭하세요.',
+			});
+			this.timeout = setTimeout(() => {
+				this.setState({
+					state: 'now',
+					message: '지금! 클릭!',
+				});
+			}, Math.floor(Math.random() * 1000) + 2000); // 2초 ~ 3초 랜덤
+		} else if (state === 'ready') {
+			// 성급하게 클릭
+			clearTimeout(this.timeout);
+			this.setState({
+				state: 'waiting',
+				message: '너무 성급합니다. 초록색이 된 후에 클릭하세요.',
+			});
+		} else if (state === 'now') {
+			// 반응속도 체크
+			this.setState({
+				state: 'waiting',
+				message: '클릭해서 시작하세요.',
+				result: [],
+			});
+		}
+	};
 
 	renderAverage = () => {
 		const { result } = this.state;
