d763a422d7cc78e40b5452b363b868f015409bff
unknown <wknowpark797@gmail.com>
2023-11-01 16:35:51 +0900

    반응속도체크 - useRef를 사용하여 this 속성 전환

--- 4.반응속도체크/ResponseCheck.jsx
+++ 4.반응속도체크/ResponseCheck.jsx
@@ -1,35 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const ResponseCheck = () => {
 	const [state, setState] = useState('waiting'); // 배경색 상태 - waiting(파란색), ready(빨간색), now(초록색)
 	const [message, setMessage] = useState('클릭해서 시작하세요.');
 	const [result, setResult] = useState([]);
+	const timeout = useRef(null);
+	const startTime = useRef(0);
+	const endTime = useRef(0);
 
 	const onClickScreen = () => {
 		if (state === 'waiting') {
-			setState('ready');
-			setMessage('초록색이 되면 클릭하세요.');
-
-			this.timeout = setTimeout(() => {
+			timeout.current = setTimeout(() => {
 				setState('now');
 				setMessage('지금! 클릭!');
-
-				this.startTime = new Date();
+				startTime.current = new Date();
 			}, Math.floor(Math.random() * 1000) + 2000); // 2초 ~ 3초 랜덤
+
+			setState('ready');
+			setMessage('초록색이 되면 클릭하세요.');
 		} else if (state === 'ready') {
 			// 성급하게 클릭
-			clearTimeout(this.timeout);
+			clearTimeout(timeout.current);
 
 			setState('waiting');
 			setMessage('너무 성급합니다. 초록색이 된 후에 클릭하세요.');
 		} else if (state === 'now') {
 			// 반응속도 체크
-			this.endTime = new Date();
+			endTime.current = new Date();
 
 			setState('waiting');
 			setMessage('클릭해서 시작하세요.');
 			setResult((prevResult) => {
-				return [...prevResult, this.endTime - this.startTime];
+				return [...prevResult, endTime.current - startTime.current];
 			});
 		}
 	};
