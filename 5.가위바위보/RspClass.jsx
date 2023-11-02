92a510a1a53cbcf40e9cecb953e85811aeccc3b6
unknown <wknowpark797@gmail.com>
2023-11-02 14:58:53 +0900

    라이프사이클 componentDidMount를 사용하여 가위바위보 이미지 변경 적용

--- 5.가위바위보/RspClass.jsx
+++ 5.가위바위보/RspClass.jsx
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
 
+const rspCoords = {
+	바위: '0',
+	가위: '-142px',
+	보: '-284px',
+};
+
+const scores = {
+	바위: 0,
+	가위: 1,
+	보: -1,
+};
+
 class RspClass extends Component {
 	state = {
 		result: '',
-		imgCoord: 0,
+		imgCoord: '0', // 좌표
 		score: 0,
 	};
+
+	interval;
+
+	// 컴포넌트 첫 렌더링 후
+	componentDidMount() {
+		this.interval = setInterval(() => {
+			const { imgCoord } = this.state; // 클로저 에러 주의
+
+			if (imgCoord === rspCoords.바위) {
+				this.setState({
+					imgCoord: rspCoords.가위,
+				});
+			} else if (imgCoord === rspCoords.가위) {
+				this.setState({
+					imgCoord: rspCoords.보,
+				});
+			} else if (imgCoord === rspCoords.보) {
+				this.setState({
+					imgCoord: rspCoords.바위,
+				});
+			}
+		}, 1000);
+	}
+
+	// 리렌더링 후
+	componentDidUpdate() {}
+
+	// 컴포넌트 제거되기 직전
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+
+	onClickBtn = () => {};
 
 	render() {
 		const { result, imgCoord, score } = this.state;
@@ -18,13 +63,13 @@ class RspClass extends Component {
 				></div>
 
 				<div>
-					<button id='rock' className='btn' onClick={() => onClickBtn('바위')}>
+					<button id='rock' className='btn' onClick={() => this.onClickBtn('바위')}>
 						바위
 					</button>
-					<button id='scissor' className='btn' onClick={() => onClickBtn('가위')}>
+					<button id='scissor' className='btn' onClick={() => this.onClickBtn('가위')}>
 						가위
 					</button>
-					<button id='paper' className='btn' onClick={() => onClickBtn('보')}>
+					<button id='paper' className='btn' onClick={() => this.onClickBtn('보')}>
 						보
 					</button>
 				</div>
