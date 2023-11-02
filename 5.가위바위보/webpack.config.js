76eb75b6b4b25576902da5f87e21a6d926f59085
unknown <wknowpark797@gmail.com>
2023-11-02 13:21:55 +0900

    가위바위보 게임 프로젝트 세팅

---
+++ 5.가위바위보/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+	name: 'rsp-setting',
+	mode: 'development',
+	devtool: 'eval',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+
+	entry: {
+		app: './client',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						[
+							'@babel/preset-env',
+							{
+								targets: {
+									browsers: ['> 5% in KR', 'last 2 chrome versions'],
+								},
+								debug: true,
+							},
+						],
+						'@babel/preset-react',
+					],
+					plugins: ['react-refresh/babel'],
+				},
+			},
+		],
+	},
+	plugins: [new ReactRefreshWebpackPlugin()],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'app.js',
+		publicPath: '/dist',
+	},
+	devServer: {
+		devMiddleware: { publicPath: '/dist' },
+		static: { directory: path.resolve(__dirname) },
+		hot: true,
+	},
+};
