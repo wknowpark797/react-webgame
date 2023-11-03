ec9e3e3af771e08050792544672ea7e8d4532283
unknown <wknowpark797@gmail.com>
2023-11-03 14:41:39 +0900

    Rsp 컴포넌트 Hooks로 전환 (useEffect 적용)

--- 5.가위바위보/client.jsx
+++ 5.가위바위보/client.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import RspClass from './RspClass';
+// import RspClass from './RspClass';
+import Rsp from './Rsp';
 
-ReactDOM.createRoot(document.querySelector('#root')).render(<RspClass />);
+ReactDOM.createRoot(document.querySelector('#root')).render(<Rsp />);
