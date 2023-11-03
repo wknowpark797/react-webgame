5cda7df5cdfdbfe81e686c3b8cd5baee706cc9a1
unknown <wknowpark797@gmail.com>
2023-11-03 13:50:54 +0900

    가위바위보 - 고차함수 변경 리팩토링

--- 5.가위바위보/RspClass.jsx
+++ 5.가위바위보/RspClass.jsx
@@ -58,7 +58,7 @@ class RspClass extends Component {
 		}
 	};
 
-	onClickBtn = (choice) => {
+	onClickBtn = (choice) => () => {
 		const { imgCoord } = this.state;
 		clearInterval(this.interval);
 
@@ -102,13 +102,13 @@ class RspClass extends Component {
 				></div>
 
 				<div>
-					<button id='rock' className='btn' onClick={() => this.onClickBtn('바위')}>
+					<button id='rock' className='btn' onClick={this.onClickBtn('바위')}>
 						바위
 					</button>
-					<button id='scissor' className='btn' onClick={() => this.onClickBtn('가위')}>
+					<button id='scissor' className='btn' onClick={this.onClickBtn('가위')}>
 						가위
 					</button>
-					<button id='paper' className='btn' onClick={() => this.onClickBtn('보')}>
+					<button id='paper' className='btn' onClick={this.onClickBtn('보')}>
 						보
 					</button>
 				</div>
